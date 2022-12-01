c1d3e8e5549f6d1695b83a15ec718e117230acd5
captainnairauk <aniket1995@yahoo.co.in>
2022-12-01 11:41:33 +0530

    Add edit & update feature.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ AlphaBlog::Application.routes.draw do
   
   get "pages/home"
   get "pages/about"
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
