dc767d4445e07ef941a0fe2aa39164d6bb547dfe
captainnairauk <aniket1995@yahoo.co.in>
2022-12-03 19:04:51 +0530

    Style articles index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,8 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require jquery
 // = require jquery_ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap.min
-
-= require jquery
-= require jquery_ujs
