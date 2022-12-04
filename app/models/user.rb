70cbfade9723202ad60b826d782104c7291ac65d
captainnairauk <aniket1995@yahoo.co.in>
2022-12-04 17:40:23 +0530

    Alter object state before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    before_save {self.email = email.downcase}
     has_many :articles
     validates :username, presence: true,
                         uniqueness: {case_sensitive: false},    
