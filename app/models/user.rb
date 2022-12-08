df3128f93140668ba19e5dc62b203135fd383fa2
captainnairauk <aniket1995@yahoo.co.in>
2022-12-08 15:26:36 +0530

    Delete users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-    has_secure_password
     before_save {self.email = email.downcase}
-    has_many :articles
+    has_many :articles, dependent: :destroy
     validates :username, presence: true,
                         uniqueness: {case_sensitive: false},    
                         length:{minimum: 3, maximum: 25}
@@ -10,5 +9,5 @@ class User < ActiveRecord::Base
                         uniqueness: {case_sensitive: false},    
                         length:{maximum: 105}, 
                         format: {with: VALID_EMAIL_REGEX}
-    
+    has_secure_password
 end
