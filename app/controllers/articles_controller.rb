dbbb87896932e741ed27fc257212e4ef583bf09c
captainnairauk <aniket1995@yahoo.co.in>
2022-12-12 18:48:07 +0530

    Update article view to display categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
+
         @article = Article.new(article_params)
         @article.user = current_user
         # render text: @article.inspect
